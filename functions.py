2b1638d3375cbb9317064736d83bb8a6b74433e3
yjh2848 <yunj05763@gmail.com>
2023-08-28 10:26:07 +0900

    test code

--- functions.py
+++ functions.py
@@ -100,13 +100,6 @@ def draw_line_through_center(img, slope):
     # 선분을 초록색으로 그리기
     cv2.line(img, (x_start, y_start), (x_end, y_end), (0, 255, 0), 2)
 
-    result_slope = round(slope, 2) #소수점 둘째자리까지 뽑아내기 result_slope = 기울기
-    #초록색 시작, 끝점 확인하는 코드
-    print("초록색 x1 : ", x_start, "초록색 y1 : ", y_start)
-    print("초록색 x2 : ", x_end, "초록색 y2 : ", y_end)
-    print("현재 초록색 기울기 : ", result_slope)
-
-
 def detect_red_cross_lines(image):
     '''
     빨간색 십자가의 형태를 감지하고 해당 선들의 각도를 계산하는 함수
@@ -114,23 +107,17 @@ def detect_red_cross_lines(image):
     angles = []  # 새로 추가한 코드: 이 부분을 함수 시작 부분에 추가
     x1, y1, x2, y2 = 0, 0, 0, 0  # 변수 초기화
 
-    # BGR에서 HSV 즉 Hue-Saturation-Value형식으로 변환 (색상, 채도, 명도)
+    # BGR -> HSV Color Transform
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        
-    # 주황색에서부터 빨간색까지 인식할 수 있게
-    lower_orange = np.array([0, 120, 70])
-    upper_orange = np.array([22, 255, 255])
-    # (이렇게 색상범위를 여러개를 한 이유는 사진으로 찍었을때 노랑과 주황으로 보이는 경우를 대비)
-    # 빨강에서부터 노랑까지 인식할 수 있게
-    lower_red = np.array([160, 120, 70])
-    upper_red = np.array([179, 255, 255])
+    
+    # 빨간색 범위의 최소,최대값 저장
+    lower_red = np.array([0, 120, 70])
+    # upper_red = np.array([10, 255, 255])
+    # upper_red = np.array([102, 228, 248])
+    upper_red = np.array([10, 255, 255])
 
-    # 두 범위의 마스크 이미지를 생성합니다.
-    mask_orange = cv2.inRange(hsv, lower_orange, upper_orange)
-    mask_red = cv2.inRange(hsv, lower_red, upper_red)
-
-    # 위에서 만든 마스크 두개를 한 마스크로 결합을 해준다
-    mask = cv2.addWeighted(mask_orange, 1, mask_red, 1, 0)
+    # 빨간색 부분을 추출하여 이진화된 마스크 이미지 추출
+    mask = cv2.inRange(hsv, lower_red, upper_red)
 
     # grayscale 변경
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -153,7 +140,8 @@ def detect_red_cross_lines(image):
         for line in lines:
             # first line
             x1, y1, x2, y2 = line[0]
-            print
+            
+            
             # 선의 중심에 위치한 픽셀이 빨간색 범위 내인지 확인
             mid_line_x = int((x1 + x2) / 2)
             mid_line_y = int((y1 + y2) / 2)
@@ -175,21 +163,15 @@ def detect_red_cross_lines(image):
                 # 수평선과 수직선 검출
                 cv2.line(output, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
-
-                # 빨간선 인식한거의 시작점과끝점에 동그라미 표시하기
-                cv2.circle(output, (x1, y1), 10, (0, 255, 0), 2)
-                cv2.circle(output, (x2, y2), 10, (0, 255, 0), 2)
-
                 float_angle = round(angle, 2) #소수점 2자리까지
                 angles.append(float_angle)  # 각도 값 저장
                 output = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
                 cv2.line(output, (x1, y1), (x2, y2), (255, 0, 0), 2)
-
+                
         # cv2.imshow("mask", mask)
         # cv2.imshow("result", output)
         cv2.waitKey(0)
-
-    return output, angles
+    return output, angles, (x1,x2) , (y1,y2)
 
 def draw_line_between_points(image, point1, point2, color):
     # 두 점 사이에 선 그리기
@@ -207,54 +189,4 @@ def mouse_handler(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:#마우스 왼쪽 버튼
         print("x좌표 : ", x)
         print("y좌표 : ", y)
-
-def detect_red_lines(img):
-
-    '''
-    
-    made by dlatldhs
-
-    이 함수는 선을 찾는 함수
-    맨 처음 이미지를 읽은 다음 복잡한 컬러 화소를 가지고 있는 이미지를
-    단순한 형태인 회색조 형태로 변환함 그리고 좀 더 처리하기 쉬운 형태인 이진화로
-    영상의 어두운 부분은 완전 어둡고 밝은 부분은 더 밝게 변함
-    여기서 Thresholud() 의 인자로 임계값을 설정할 수 있음 카메라 성능에 따라 적절한 임계값 필요
-    canny() 함수로 흰색과 검은색의 경계선을 추출함( 윤곽선 )
-    윤곽선 중 직선으로 된 선을 추출합니다.
-    반환되는 값으로는 리스트로 감지된 값의 시작점과 끝점을 반환합니다.
-    예시) [ (x1,y1,x2,y2) , (x1,y1,x2,y2) ...] 이런식으로 인식한 선의 좌표를 반환함
-    참고 자료 : https://blog.naver.com/suresofttech/221606239357
-
-    '''
-    # image read
-    image = img.copy()
-
-    # image 회색조 변환
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-    # 회색조 -> 이진화
-    # _, binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-    # 블러로 부드럽게 ~ 
-    gauss = cv2.GaussianBlur(gray, (5,5), 0)
-
-    # 이진화 된 사진으로 윤곽선 검출
-    edges = cv2.Canny(gauss, 50, 150)
-
-    # 직선 검출하기
-
-    # rho : 검출할 선분의 거리 해상도(픽셀 단위) 보통 1사용
-    # theta : 세타 (각도)
-    # threshold : 투표(vote)를 위한 최소 공간으로 임계값과 비슷함
-    # 이 값보다 큰 값만 직선으로 검출됨, 일반적으로 검출할 선분에 개수에 따라 조정됨
-    # 값이 작으면 많이 검출, 값이 크면 더 적은 선분 검출
-
-    # minLinelength : 선분의 최소길이(픽셀 단위) 이 짧은 선분은 무시됨
-    # maxLineGap : 끊어진 선분 사이의 최대 허용 간격(픽셀 단위)
-    # maxLineGap 보다 작은 간격이 있는 선분은 결합되어 하나의 선으로 됨
-
-    lines = cv2.HoughLinesP(edges, rho=1, theta=1*np.pi/180,
-                             threshold=50,
-                             minLineLength=10,
-                             maxLineGap=250)
-    
-    return lines
+ 
---
+++ test2.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+import functions
+
+def draw_line_through_center(img, slope):
+    h, w = img.shape[:2]
+    cx, cy = w // 2, h // 2
+
+    # y절편 계산
+    b = cy - slope * cx
+
+    # 두 점의 좌표를 계산
+    x_start = 0
+    y_start = int(slope * x_start + b)
+    x_end = w
+    y_end = int(slope * x_end + b)
+
+    # 선분을 초록색으로 그리기
+    cv2.line(img, (x_start, y_start), (x_end, y_end), (0, 255, 0), 2)
+
+# 이미지 읽기
+img_path = "yunjonghyeok.png"
+image = cv2.imread(img_path)
+
+# h, w, _ = img.shape
+   # 정수 나눗셈 사용하여 반올림 처리
+   # 정수 나눗셈 사용하여 반올림 처리
+# image = cv2.resize(img, (w,h))
+
+angle =61.98   # 각도 대입: 54.98°와 -26.19° 설정 예시입니다.
+angle2 =-28.19   # 각도 대입: 54.98°와 -26.19° 설정 예시입니다.
+slope = np.tan(angle * np.pi / 180.0)
+slope2= np.tan(angle2 * np.pi /180.0)
+
+# 이미지에 기울기가 x인 선분 그리기
+draw_line_through_center(image,slope)
+draw_line_through_center(image,slope2)
+
+crossline_img, angles, (x1,x2), (y1,y2) = functions.detect_red_cross_lines(image)
+cv2.circle(crossline_img, (x1, x2), 5, (0, 255, 0), -1)
+cv2.circle(crossline_img, (y1, y2), 5, (0, 255, 0), -1)
+while True:
+	cv2.imshow("Result", image)
+	cv2.imshow("Result2", crossline_img)
+	key = cv2.waitKey(1) 
+	if key == ord('c'):   # 'c' 키를 누르면 사진 캡처 및 저장 수행합니다.
+		cv2.imwrite('cap_img.png', image)
+		
+
+cv.destroyAllWindows()
