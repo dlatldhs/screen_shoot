496df76ec1b08fc230c3f359d87f34ba4d2860e6
dlatldhs <sionzz1713@gmail.com>
2023-08-28 19:40:37 +0900

    aaaaaaaaaaaaaaaaaaa

--- dlatldhs.py
+++ dlatldhs.py
@@ -9,7 +9,7 @@ def main():
 
     # line angle save
     line_angle_save = []
-    line_list = []
+    red_line_list = []
     c = 2
 
     # capture img
@@ -25,27 +25,10 @@ def main():
         if c > 0:
             c -= 1
             # print(type(line[0]))
-            line_list.append(line[0])
+            red_line_list.append(line[0])
 
-    dots_img = functions.draw_dots(image,line_list)
-    
-    # functions.draw_line_through_center(image, line_angle_save)
-    # functions.draw_dot(img=copy,x=581,y=1)
-    # functions.draw_dot(img=copy,x=809,y=411)
-
-    # functions.draw_dot(img=copy,x=0,y=261)
-    # functions.draw_dot(img=copy,x=466,y=2)
-
-    # lines = functions.detect_red_lines(image)
-    # img = image.copy()
-    # result = functions.draw_lines(img,lines)
-
-    # cv2.imshow('dots',dots_img)
+    _, = functions.draw_dots(image,red_line_list)
     cv2.imshow('image',image)
-    # cv2.imshow('mask ',mask)
-    # cv2.imshow('copy',copy)
-    
-    # cv2.imshow('result',result)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
